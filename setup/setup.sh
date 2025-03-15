76ddbcc0e462b2e70199f7f8557c2a7d08a864b1
Benedikt Schesch <b.schesch@gmail.com>
2025-03-15 15:41:50 -0700

    Revert to https

--- setup/setup.sh
+++ setup/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 apt-get install screen -y
-git clone git@github.com:benedikt-schesch/LLMerge.git
+git clone https://github.com/benedikt-schesch/LLMerge.git
 cd LLMerge
 pip install uv
 uv sync
