fbbcc61ddea9bf397ba6e558cc0e358946fef494
Benedikt Schesch <b.schesch@gmail.com>
2025-04-07 13:55:51 -0700

    Eval all checkpoints script

--- eval_all_checkpoints.sh
+++ eval_all_checkpoints.sh
@@ -4,6 +4,6 @@
 for dir in outputs/checkpoint-*; do
   if [ -d "$dir" ]; then
     echo "Running eval.py on $dir"
-    python3 eval.py --model_name "$dir" --lora_weights "$dir"
+    python3 eval.py --lora_weights "$dir"
   fi
 done
